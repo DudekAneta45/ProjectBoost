8eca7986640d976c7cc3423970ccaccfd6c43def
An\Anett <dudka45@gmail.com>
2019-11-26 15:59:31 +0100

    Adding full game again because of problems with git lfs data limit on previous version

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,176 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Rocket : MonoBehaviour {
+
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float delay = 3f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip death;
+    [SerializeField] AudioClip success;
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] GameObject spider;
+    [SerializeField] ParticleSystem explosionParticles;
+    [SerializeField] AudioClip spiderDeathSFX;
+    [SerializeField] GameObject button;
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    bool isAlive = true;
+    bool collisionDisabled = false;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(isAlive)
+        { 
+            RespondToMovingUpInput();
+            RespondToRotateInput();
+        }
+        if(Debug.isDebugBuild) // work only when in BuildSettings "Development Build" in ON
+        { 
+            RespondToDebugKey();
+        }
+
+        if(Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!isAlive || collisionDisabled)
+        {
+            return;
+        }
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                //do nothing
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            case "Interact":
+                StartCoroutine(DestroySpider());
+                button.tag = "Friendly";
+                button.GetComponent<ParticleSystem>().Stop();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+    }
+    IEnumerator DestroySpider()
+    {
+        isAlive = false;
+        audioSource.Stop();
+        audioSource.PlayOneShot(spiderDeathSFX);
+        explosionParticles.Play();
+        Object.Destroy(spider, 1.5f);
+        yield return new WaitForSeconds(1);
+        isAlive = true;
+    }
+
+    private void StartDeathSequence()
+    {
+        isAlive = false;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        deathParticles.Play();
+        Invoke("LoadFirstLevel", delay);
+    }
+
+    private void StartSuccessSequence()
+    {
+        isAlive = false;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        successParticles.Play();
+        Invoke("LoadNextLevel", delay); // delaying loading next scene
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0; // loop back to start
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    void RespondToRotateInput()
+    {
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            RotateManually(rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            RotateManually(-rotationThisFrame);
+        }
+    }
+
+    private void RotateManually(float rotationThisFrame)
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+        transform.Rotate(Vector3.forward * rotationThisFrame);
+        //transform.Rotate(0, 0, 1 * rotationThisFrame);
+        rigidBody.freezeRotation = false; //resume physics control of rotation
+    }
+
+    private void RespondToMovingUpInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
+        {
+            ApplyMovingUp();
+        }
+        else
+        {
+            audioSource.Stop();
+            mainEngineParticles.Stop();
+        }
+    }
+
+    private void ApplyMovingUp()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust); // moving up
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        mainEngineParticles.Play();
+    }
+
+    private void RespondToDebugKey() 
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            collisionDisabled = !collisionDisabled; //toggle
+        }
+    }
+}
---
+++ Assets/Scripts/RotateObstacles.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[DisallowMultipleComponent]
+public class RotateObstacles : MonoBehaviour
+{
+
+    [SerializeField] Vector3 rotateVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f; // time of one full circle
+
+    //todo remove from inspector later
+    float rotateFactor; //0 for not moved, 1 for fully moved
+    Vector3 startingPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        startingPos = transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (period <= Mathf.Epsilon) //Epsilon is the smallest float
+        {
+            return;
+        }
+        float cycles = Time.time / period; //grows continually from 0
+        const float tau = Mathf.PI * 2; //about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau); //goes from -1 to +1
+
+        rotateFactor = rawSinWave / 2f + 0.5f; // goes from 0 to 1
+        Vector3 offset = rotateVector * rotateFactor;
+        transform.eulerAngles = startingPos + offset;
+    }
+}
+
